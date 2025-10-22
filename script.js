17ec18a5ded720519ba83e41b94d3f32e32801b9
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-10-22 21:47:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete the given function
 function palindrome(str){
-	return !!str.split(" ").reverse().join(" ")
-
+	const cleanedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+	return cleanedStr === cleanedStr.split('').reverse().join('');
 }
 
 let sentence = prompt("Enter a sentence");
