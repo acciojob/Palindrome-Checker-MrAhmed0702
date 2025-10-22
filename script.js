930d0e87beae587d03c3c8f3d543f048cc3cc91d
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-10-22 22:12:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-// complete the given function
-function palindrome(str){
-	const cleanedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
-	return cleanedStr === cleanedStr.split('').reverse().join('');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function palindrome(str) {
+    const cleanedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    return cleanedStr === cleanedStr.split('').reverse().join('');
 }
 
-let sentence = prompt("Enter a sentence");
+// Ask the user for input using the readline interface
+rl.question('Enter a sentence: ', (sentence) => {
+    if (palindrome(sentence)) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
 
-if(palindrome(sentence)) alert(true)
-else alert(false);
+    rl.close();
+});
